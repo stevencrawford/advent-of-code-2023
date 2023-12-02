f986335bfd0f7077def6b81d75d5accfdb10d7af
Steven Crawford <steven.crawford.uk@gmail.com>
2023-12-02 23:08:12 +0100

    complete day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,15 +1,41 @@
+type StringNumber = [string, number];
+const numbers: StringNumber[] = [['1', 1], ['2', 2], ['3', 3], ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9]];
+const words: StringNumber[] = [['one', 1], ['two', 2], ['three', 3], ['four', 4], ['five', 5], ['six', 6], ['seven', 7], ['eight', 8], ['nine', 9]];
+const numbersAndWords: StringNumber[] = [...numbers, ...words];
+
+const minComparator = (a: [StringNumber, [number, number]], b: [StringNumber, [number, number]]) => a[1][0] - b[1][0];
+const maxComparator = (a: [StringNumber, [number, number]], b: [StringNumber, [number, number]]) => a[1][1] - b[1][1];
+
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  return calculate(input, numbers);
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = '54597';
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  return calculate(input, numbersAndWords);
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = '54504';
+
+const calculate = (input: string, numbersToFind: StringNumber[]) => {
+  return input.split('\n')
+    .map((line) => {
+      let digitsWithIndexes: [StringNumber, [number, number]][] = numbersToFind
+        .map(([digit, _number]): [StringNumber, [number, number]] => [[digit, _number], [line.indexOf(digit), line.lastIndexOf(digit)]])
+        .filter(([_, [min, max]]) => min > -1 && max > -1);
+
+      return digitsWithIndexes
+        .reduce((acc, current) => {
+          acc[0] = minComparator(acc[0], current) < 0 ? acc[0] : current;
+          acc[1] = maxComparator(acc[1], current) > 0 ? acc[1] : current;
+          return acc;
+        },
+          [digitsWithIndexes[0], digitsWithIndexes[0]])
+        .reduce((accumulator, [digitAsStrings, index]) => 10 * (accumulator) + digitAsStrings[1], 0)
+        ;
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
