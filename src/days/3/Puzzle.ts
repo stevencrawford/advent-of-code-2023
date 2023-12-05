8626aedc0158fa7850b85de7767a9e172018fd4b
Steven Crawford <steven.crawford.uk@gmail.com>
2023-12-05 21:38:31 +0100

    day 3 still not working (passes all online testcases but fails AOC input!!!)

--- src/days/3/Puzzle.ts
+++ src/days/3/Puzzle.ts
@@ -1,62 +1,70 @@
+import { start } from "repl";
+
 type Symbol = "!" | "@" | "#" | "$" | "%" | "^" | "&" | "*" | "(" | ")" | "_" | "+" | "-" | "=" | "[" | "]" | "{" | "}" | "\\" | "|" | ";" | ":" | "'" | "\"" | "," | "<" | ">" | "/" | "?";
-type IndexedValue = { value: number | Symbol; startIndex: number; endIndex: number }
-type IndexedLine = { numbers: IndexedValue[]; symbols: IndexedValue[] }
+type Indexed = { startIndex: number; endIndex: number }
+type IndexedAny = { value: number | Symbol } & Indexed
+type IndexedNumber = { value: number } & Indexed
+type IndexedSymbol = { value: Symbol } & Indexed
+type IndexedLine = { line: number; numbers: IndexedNumber[]; symbols: IndexedSymbol[] }
 
 const NUM_BUFFER = 1;
 const SYMBOL_BUFFER = 0;
 
 const numberRegex: RegExp = /\d+/g;
-const symbolRegex: RegExp = /[!@#$%^&*()_+\-=\[\]{}\\|;:'",<>\/?]/g;
+const symbolRegex: RegExp = /[^0-9.]/g; // not a digit or a period
 
 type Processor<T> = (value: string) => T;
 
 const numberProcessor: Processor<number> = (value: string) => parseInt(value, 10);
 const symbolProcessor: Processor<Symbol> = (value: string) => value as Symbol;
 
-const extractValues = (text: string, pattern: RegExp, buffer: number, processor: Processor<number | Symbol>): IndexedValue[] => {
-  let result: IndexedValue[] = [];
+const extractValues = (text: string, pattern: RegExp, buffer: number, processor: Processor<number | Symbol>): IndexedAny[] | IndexedSymbol[] => {
+  let result: IndexedAny[] = [];
   let match: RegExpExecArray | null;
   while ((match = pattern.exec(text)) !== null) {
     const [fullMatch] = match;
     const startIndex = Math.max(match.index - buffer, 0);
-    const endIndex = Math.min((startIndex + (fullMatch.length - 1)) + 2 * buffer, text.length);
+    const endIndex = Math.min(((startIndex == 0) ? startIndex - 1 : startIndex) + fullMatch.length + buffer, text.length);
 
     result.push({ value: processor(fullMatch), startIndex, endIndex });
   }
   return result;
 }
 
-const findCollisions = (numbers: IndexedValue[], symbols: IndexedValue[]): IndexedValue[] => {
-  return numbers.filter(number => symbols.some(_symbol => _symbol.startIndex >= number.startIndex && _symbol.endIndex <= number.endIndex));
+const findAdjacentWithSymbol = (numbers: IndexedNumber[], symbols: IndexedSymbol[]): IndexedNumber[] => {
+  return numbers.filter(number => symbols.some(_symbol => _symbol.startIndex >= number.startIndex && _symbol.startIndex <= number.endIndex));
 }
 
 const first = (input: string) => {
   const indexedLines: IndexedLine[] = [];
   input.split('\n')
-    .map((line) => {
+    .map((line, index) => {
       let indexedLine = {
-        numbers: extractValues(line, numberRegex, NUM_BUFFER, numberProcessor),
-        symbols: extractValues(line, symbolRegex, SYMBOL_BUFFER, symbolProcessor)
+        line: index + 1,
+        numbers: extractValues(line, numberRegex, NUM_BUFFER, numberProcessor) as IndexedNumber[],
+        symbols: extractValues(line, symbolRegex, SYMBOL_BUFFER, symbolProcessor) as IndexedSymbol[]
       }
       indexedLines.push(indexedLine);
     })
 
-  let collisions: IndexedValue[] = []
+  let partNumbers: IndexedNumber[] = []
   indexedLines.forEach((line, index, lines) => {
+    let adjacent: IndexedNumber[] = []
     // check for collisions on the line above (if not at first line)
     if (index - 1 >= 0) {
-      // console.log(original[index - 1])
-      collisions.push(...findCollisions(line.numbers, lines[index - 1].symbols))
+      adjacent.push(...findAdjacentWithSymbol(line.numbers, lines[index - 1].symbols))
     }
     // check for collisions on the same line
-    collisions.push(...findCollisions(line.numbers, line.symbols))
+    adjacent.push(...findAdjacentWithSymbol(line.numbers, line.symbols))
     // check for collisions with the line below (if not already at last line)
     if (index + 1 < lines.length) {
-      collisions.push(...findCollisions(line.numbers, lines[index + 1].symbols))
+      adjacent.push(...findAdjacentWithSymbol(line.numbers, lines[index + 1].symbols))
     }
+    // remove duplicates where number has more than 1 symbol adjacent to it
+    partNumbers.push(...Array.from(new Map(adjacent.map(c => [c.value + '|' + c.startIndex + '|' + c.endIndex, c])).values()));
   })
 
-  return collisions.reduce((accumulator, current) => accumulator + (current.value as number), 0);
+  return partNumbers.reduce((accumulator, current) => accumulator + current.value, 0);
 };
 
 const expectedFirstSolution = 'solution 1';
